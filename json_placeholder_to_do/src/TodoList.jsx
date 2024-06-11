b6ee06aea820482c5c60f6dbdaf0a559397ef808
Hanna-alex <ipantevaanna@gmail.com>
2024-06-11 23:06:19 +0300

    change files

--- json_placeholder_to_do/src/TodoList.jsx
+++ json_placeholder_to_do/src/TodoList.jsx
@@ -1,25 +1,27 @@
 import { useEffect, useState } from 'react'
-
 import styles from './todoList.module.css'
 
 export const TodoList=()=>{
 
 	const [todos, setTodos] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
 
 	useEffect(()=>{
+		setIsLoading(true)
 		fetch('https://jsonplaceholder.typicode.com/todos')
 		.then((response)=> response.json())
 		.then((todos)=>{
 			setTodos(todos)
 		})
 		.catch((error)=> console.error(error))
+		.finally(setIsLoading(false))
 	},[])
-
-
 
 	return (
 		<ul className={styles.todoList}>
-			{todos.map(({id, title,completed}) => (
+			{isLoading ?
+			<div className={styles.loader}></div>
+			:todos.map(({id, title,completed}) => (
 					<li className={styles.item +' '+  (completed ? styles.itemDone: '')} key={id}>{title} </li>
 			))}
 
--- json_placeholder_to_do/src/index.js
+++ json_placeholder_to_do/src/index.js
@@ -6,7 +6,6 @@ import { TodoList } from './TodoList.jsx'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-
 		<TodoList />
 	</React.StrictMode>
 )
